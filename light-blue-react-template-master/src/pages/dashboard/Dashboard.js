edb8f758ff75cf30204b0ff0156872589399065e
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2020-09-06 05:08:32 -0400

    marker content and icon code

--- light-blue-react-template-master/src/pages/dashboard/Dashboard.js
+++ light-blue-react-template-master/src/pages/dashboard/Dashboard.js
@@ -36,10 +36,25 @@ class Dashboard extends React.Component {
     this.state = {
       graph: null,
       checkedArr: [false, false, false],
+      markerType: 'POI',
+      content: ''
     };
     this.checkTable = this.checkTable.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.onEdit = this.onEdit.bind(this);
   }
+  handleChange(e) {
+    let {name, value} = e.target;
+    this.setState({
+      markerType: value
+    });
 
+  }
+  onEdit(event){
+    console.log(this.state.content);
+    this.setState({[event.target.name] : event.target.value})
+  }
+  content
   checkTable(id) {
     let arr = [];
     if (id === 0) {
@@ -72,11 +87,11 @@ class Dashboard extends React.Component {
       <div className={s.root}>
 
         <Row>
-          <Map />
+          <Map markerType = {this.state.markerType} markerContent = {this.state.content}/>
               <Col lg={2}>
               <FormGroup>
                <Label>Select</Label>
-               <Input type="select" name="select">
+               <Input type="select" name="select" onChange = {this.handleChange}>
                  <option>POI</option>
                  <option>Casual</option>
                  <option>Urgent</option>
@@ -84,7 +99,7 @@ class Dashboard extends React.Component {
              </FormGroup>
              <FormGroup>
               <Label>Content</Label>
-              <Input type="text" name="Marker-content" placeholder="Content" />
+              <Input type="text" name="Marker-content" placeholder="Content" onChange = {this.onEdit} name = "content" value = {this.state.content}/>
             </FormGroup>
             </Col>
 
